b8bd312a02452e8f6ff1856727ec8205ef913bcf
Mate Bek <mate.bek@transferwise.com>
2021-10-05 16:11:32 +0200

    test: add expanded story variant to Card

--- packages/components/src/card/Card.story.js
+++ packages/components/src/card/Card.story.js
@@ -9,12 +9,13 @@ export default {
   title: 'Card',
 };
 
-export const Basic = () => {
+const Template = (props) => {
   const elementType = select('elementType', ['div', 'li'], 'div');
   const title = text('title', 'A card');
   const details = text('details', 'Some details about this card');
   const content = text('content', 'Lorem ipsum dolor sit amet.');
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(props.isExpanded);
+
   return (
     <Card
       as={elementType}
@@ -27,4 +28,12 @@ export const Basic = () => {
       {content}
     </Card>
   );
+};
+
+export const Basic = () => {
+  return <Template isExpanded={false} />;
+};
+
+export const Expanded = () => {
+  return <Template isExpanded />;
 };
